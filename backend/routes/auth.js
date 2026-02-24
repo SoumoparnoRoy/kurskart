3517752212d418530442496e82f3b7f76d1e5f82
Soumoparno Roy <soumoparnoroy2006@gmail.com>
2026-02-25 00:59:53 +0530

    Add signup and signin routes with JWT auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
 const authRouter = express.Router();
@@ -18,7 +19,35 @@ authRouter.post('/api/signup', async (req, res) => {
 
             let user = new User({ fullName, email, password: hashedPassword });
             user = await user.save();
-            res.json({ user });
+            const userObj = user._doc;
+            delete userObj.password;
+            res.json(userObj);
+        }
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const foundUser = await User.findOne({ email });
+        if (!foundUser) {
+            return res.status(400).json({ msg: "User not found with this email" });
+        } else {
+            const isMatch = await bcrypt.compare(password, foundUser.password);
+            if (!isMatch) {
+                return res.status(400).json({ msg: "Incorrect Password" });
+            } else {
+                const token = jwt.sign({ id: foundUser._id }, process.env.JWT_SECRET,
+                    { expiresIn: "7d" });
+
+                const { password, ...userWithoutPassword } = foundUser._doc;
+
+                res.json({ token, ...userWithoutPassword });
+            }
         }
     } catch (e) {
         res.status(500).json({ error: e.message });
